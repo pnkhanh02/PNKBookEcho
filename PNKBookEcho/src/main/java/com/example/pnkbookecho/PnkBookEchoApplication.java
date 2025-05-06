8363af30b49fd349616a114eef0f8d5034fb7ab7
pnkhanh02 <115911423+pnkhanh02@users.noreply.github.com>
2025-05-06 22:04:29 +0700

    first commit

---
+++ PNKBookEcho/src/main/java/com/example/pnkbookecho/PnkBookEchoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pnkbookecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnkBookEchoApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(PnkBookEchoApplication.class, args);
+    }
+
+}
---
+++ PNKBookEcho/src/test/java/com/example/pnkbookecho/PnkBookEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pnkbookecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnkBookEchoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
